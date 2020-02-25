7d05af855f3258ff67358b5842b9d0a141d9b40f
Ryan Isaacson <munchinator6354@gmail.com>
2020-02-24 21:04:15 -0800

    added message, npm i console.table, need to reduce database quantity by quantity selected next

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,8 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+// var cTable = 
+require("console.table");
+
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -17,30 +20,68 @@ connection.connect(function (err) {
 function displayMarket() {
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
-        inquirer
-            .prompt([
+        console.table(results);
+        inquirer.prompt([
                 {
-                    name: "choice",
-                    type: "rawlist",
-                    message: "------------------------------\n Welcome to the Bamazon Market. Below are the available items. \n ------------------------------",
+                    type: "input",
+                    name: "whichID",
+                    message: "------------------------------\n Please select the ID of the item you would like to buy from the table above. \n ------------------------------",
                     choices: function () {
                         var productsArray = [];
                         for (var i = 0; i < results.length; i++) {
                             productsArray.push(results[i].product_name);
                         }
+                        console.log(productsArray + " This is productsArray");
                         return productsArray;
-                    }
+                    },
+
+                },
+                    
+                    {
+                        type: "input",
+                        name: "howMany",
+                        message: "------------------------------\n How many of that item would you like? \n ------------------------------",
+                        choices: function () {
+                            var productsArray = [];
+                            for (var i = 0; i < results.length; i++) {
+                                productsArray.push(results[i].product_name);
+                            }
+                            return productsArray;
+                        },
                 }
             ])
-            .then(function (answer) {
-                var chosenProduct;
-                for (var i = 0; i < results.length; i++) {
-                    if (results[i].product_name === answer.choice) {
-                        chosenItem = results[i];
-                    }
-                }
+            .then(answer => {
+                console.log(Object.values(answer) + " This is answer");
+                // console.log(answer.whichID)
+                var chosenProduct = parseInt(answer.whichID);
+                var chosenQuantity = parseInt(answer.howMany);
+                console.log(typeof chosenProduct)
+                var productExists = checkIfInTable(results, chosenProduct);
+                console.log(productExists);
+                enoughQuantity(productExists, chosenQuantity);
             });
-    });
-}
+    })
+
+// when creating a function, you can use a placeholder title/word for what is being passed through the function. When you are actually calling or invoking the function, it must specifically pass what you are intending.
+function checkIfInTable(results, itemID) {
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].item_id === itemID) {
+                    return results[i];
+                }
+            }
+            return null;
+        };
+
+    }
+
+function enoughQuantity(productExists, chosenQuantity) {
+    console.log(productExists)
+    if (productExists.stock_quantity < chosenQuantity) {
+        console.log("Insufficient Quantity");
+    }
+    else connection.query("SELECT * FROM products WHERE product_name = " + productExists.product_name
+};
 
 
+    
+    // Have an if statement afte rline 43 that if prouctExists, call another function prompting quantity, passproductExists into the function and then start the process of inquirer, use input, ask them how many they want to buy, then make purchase function (doing another query) to update the database/table.
